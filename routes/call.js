b178d23aa3c4017160008892ec8b9938e9989443
Vibhu Dixit <vibhudixit.2215001940@gmail.com>
2024-11-21 11:31:29 +0530

    add payment/frontend

--- routes/call.js
+++ routes/call.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const User = require('../models/User');
+const Lawyer = require('../models/Lawyer');
 const router = express.Router();
 
 router.get('/Call', async (req, res) => {
     if (!req.session.user) return res.redirect('/login');
     const user = await User.findById(req.session.user._id);
-    res.render('Call/index', { user });
+    const lawyer = await Lawyer.find();
+    res.render('Call/index', { user, lawyer });
 });
+router.get('/Payment', async (req, res) => {
+    res.render('Call/payment');
+})
 module.exports = router;
