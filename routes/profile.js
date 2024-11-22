6e060e909f89cf6bfef2a5c05dde17fb9ab4fe1a
Vibhu Dixit <vibhudixit.2215001940@gmail.com>
2024-11-23 01:53:26 +0530

    add complains/tracking

--- routes/profile.js
+++ routes/profile.js
@@ -4,7 +4,6 @@ const Complain = require('../models/Complain');
 const router = express.Router();
 const moment = require('moment-timezone');
 
-// Custom ensureAuthenticated middleware
 function ensureAuthenticated(req, res, next) {
     if (req.session && req.session.user) {
         return next();
@@ -12,29 +11,26 @@ function ensureAuthenticated(req, res, next) {
     res.redirect('/login');
 }
 
-// Route to render Profile page
 router.get('/Profile', async (req, res) => {
     if (!req.session.user) return res.redirect('/login');
     try {
         const user = await User.findById(req.session.user._id);
         user.lastLogin = moment(user.lastLogin).tz('Asia/Kolkata').format('YYYY-MM-DD HH:mm:ss');
         const complainCount = await Complain.countDocuments({ email: user.email });
-
-        res.render('Profile/index', { user, complainCount });
+        const resolvedCount = await Complain.countDocuments({ email: user.email, status: 'Resolved' });
+        res.render('Profile/index', { user, complainCount, resolvedCount });
     } catch (err) {
-        console.error(err);
-        res.status(500).send('Internal Server Error');
+        res.render('404/index')
     }
 });
 
-// Route to render user's complaints
 router.get('/Profile/complaints', ensureAuthenticated, async (req, res) => {
     try {
-        const complaints = await Complain.find({ userId: req.session.user._id });
+        const userEmail = req.session.user.email; 
+        const complaints = await Complain.find({ email: userEmail }); 
         res.render('Profile/complain.ejs', { complaints });
     } catch (err) {
-        console.error(err);
-        res.status(500).send('Internal Server Error');
+        res.render('404/index')
     }
 });
 
